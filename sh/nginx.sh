bb6d6dc5be88f18e90155d340214b6932f26fe87
yxm <yxm.dev@gmx.ie>
2025-02-26 23:06:37 -0300

    ...

--- sh/nginx.sh
+++ sh/nginx.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y nginx
 sudo ln -s /etc/nginx  $HOME/nginx
 sudo cp ${BASH_SOURCE%/*}/../files/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp ${BASH_SOURCE%/*}/../files/nginx/default.nginx.conf /etc/nginx/default.conf
+sudo sed -i "s/{USER}/$USER/g" /etc/nginx/nginx.conf
 sudo mkdir -p /etc/nginx/log
 sudo mkdir -p /etc/nginx/ssl
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=US/ST=State/L=City/O=Organization/OU=OrgUnit/CN=example.com"
